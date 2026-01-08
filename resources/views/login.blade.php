8cbf6bf77cbdc823fddc5d913fd659e9ec3949d4
LeonardoPilotti <pilottileonardo3@gmail.com>
2026-01-08 12:29:55 -0300

    feat(auth) implement login form and authentication logic

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,5 +3,37 @@
         <h1>
             Faça login
         </h1>
+
+        <section class="mt-4">
+            <form action="/login" method="POST">
+                @csrf
+
+            @error('email')
+                <p class="text-red-500 text-xl mt-1">
+                    {{ $message }}
+                </p>
+            @enderror
+
+             <!--Email input-->
+            <input
+            type="email"
+            name="email"
+            placeholder="your@email.com"
+            class="bg-white border-2 p-2">
+
+            <input
+            type="password"
+            name="password"
+            placeholder="********"
+            class="bg-white border-2 p-2">
+
+            <button
+            type="submit"
+            class="bg-whit border-2 p-2">
+            Entrar
+            </button>
+            </form>
+
+        </section>
     </main>
 </x-layout>
