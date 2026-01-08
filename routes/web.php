c6be80b3f976a9de9051bdc074e25832a26d943d
LeonardoPilotti <pilottileonardo3@gmail.com>
2026-01-08 11:43:36 -0300

    feat(auth) add login funcionality and user section management

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
-
+//Site
 Route::get('/',[SiteController::class, 'index']);
+
+//Login
+Route::get('/login',[LoginController::class,'index']);
