2f795fe82fb0127365dc22e22ea3553d29ac15b9
LeonardoPilotti <pilottileonardo3@gmail.com>
2026-01-19 14:06:14 -0300

    refactor(Habit): create 'wasCompletedOn' and 'generateYearGrid'

--- app/Models/Habit.php
+++ app/Models/Habit.php
@@ -2,33 +2,64 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\belongsTo;
 use Illuminate\Database\Eloquent\Relations\hasMany;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use Carbon\Carbon;
 
 class Habit extends Model
 {
     use HasFactory;
+
     protected $fillable = ['user_id', 'name'];
 
-    //Um hábito pertence a um usuário
-    public function user():belongsTo
+    // Um hábito pertence a um usuário
+    public function user(): belongsTo
     {
         return $this->belongsTo(User::class);
-    }   
+    }
 
-    //Um hábito pode ter muitos registros
+    // Um hábito pode ter muitos registros
     public function habitLogs(): hasMany
     {
         return $this->hasMany(HabitLog::class);
     }
 
-    public function wasCompletedToday():bool
+    public function wasCompletedToday(): bool
     {
         return $this->habitLogs
             ->where('completed_at', \Carbon\Carbon::today()->toDateString())
             ->isNotEmpty();
+    }
+
+    public function wasCompletedOn(Carbon $date): bool
+    {
+        return $this->habitLogs
+            ->where('completed_at', $date->toDateString())
+            ->isNotEmpty();
+    }
+    public static function generateYearGrid(int $year): array
+    {
+        $startDate = \Carbon\Carbon::create($year, 1, 1);
+        $endDate = \Carbon\Carbon::create($year, 12, 31);
+
+        $weeks = [];
+        $currentWeek = [];
+
+        $firstDayOfWeek = $startDate->dayOfWeek;
+        for ($i = 0; $i < $firstDayOfWeek; $i++) {
+            $currentWeek[] = null;
+        }
+
+        for ($date = $startDate->copy(); $date->lte($endDate); $date->addDay()) {
+            $currentWeek[] = $date->copy();
+
+            if ($date->isSaturday() || $date->eq($endDate)) {
+                $weeks[] = $currentWeek;
+                $currentWeek = [];
+            }
+        }
+        return $weeks;
     }
 }
